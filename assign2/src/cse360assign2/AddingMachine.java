7988c9bb07146458003e95c1daa071fac0744dc2
dosborn <dosborn@L0004205.internal.mcmcg.com>
2019-10-09 20:27:26 -0700

    Initial setup of methods and documentation for calculator.

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ * @author Daucen Osborn #1212941663
+ * @version 1.0
+ * Class# 85141
+ * Assignment #2
+ * Description: The purpose of this program is to practice version control
+ *  systems and to create a program that calculates inputed values
+ *  and keeps track of them.
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * 
+	 * Set total variable.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * @return total  Returns value of the total variable at the current time.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value  Adds the parameter to the 'total' variable.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value  Subtracts the value from the 'total' variable.
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * 
+	 * @return String  Shows the work of the calculator to be printed.
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * 
+	 * Resets the total value to 0 so a new value can be calculated.
+	 */
+	public void clear() {
+	
+	}
+}
